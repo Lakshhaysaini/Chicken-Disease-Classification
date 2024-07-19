103957b537ecee78751baf672ec62fe0e38aca7b
lakshhay_saini <lakshaysaini71@gmail.com>
2024-07-19 01:01:02 +0100

    data ingestion 50%

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+# we will be only reading these file one time so i.e it is declared in the constants
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path('params.yaml')
