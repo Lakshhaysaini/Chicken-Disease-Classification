f59c13ab0b43f97ddcc8a60af7a6f4b3d07b9914
lakshhay_saini <lakshaysaini71@gmail.com>
2024-07-24 00:19:32 +0100

    setting up as modular coding style

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info('welcome to my custom log')
+STAGE_NAME = 'Data Ingestion Stage'
+
+if __name__ == '__main__':
+    try:
+        logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<')
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<< \n\n X=============X')
+    except Exception as e:
+        logger.exception(e)
+        raise e
